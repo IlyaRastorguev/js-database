ae455d6bbe401d2858975c4b4d933a81a910a103
rastorguevila <rastorguew94@gmail.com>
2021-06-03 17:57:14 +0300

    fix export

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { DataBaseExecutor } from "./core/executor";
 import { StorageType } from "./types";
 
 export { BaseStorage } from "./core/storage";
-export * from "./react";
-export * from "./vanilla";
+export { useStorageEvent, useStorageInit } from "./react";
+export { subscribeForChanges, subscribeForDatabaseReady } from "./vanilla";
 
 export const initializeDatabase = (
   dataBaseName: string,
