bcd5eee211b0397e8da0128a92f9daa840244a7b
Расторгуев Илья Анатольевич <Rastorguev.I.Ana@sberbank.ru>
2021-06-11 09:44:08 +0300

    fix merge conflicts

--- src/core/executor.ts
+++ src/core/executor.ts
@@ -19,9 +19,9 @@ export class DataBaseExecutor extends EventHandler {
         this.storages = new Set([...storages]);
         const request = this.indexeddb.open(this.dbInstanceName, version);
 
-    this.storages.forEach((storage) => {
-      storage.init(this);
-    });
+        this.storages.forEach((storage) => {
+            storage.init(this);
+        });
 
         request.onsuccess = () => {
             this.fireEvent(DataBaseReadyEvent, {status: true});
